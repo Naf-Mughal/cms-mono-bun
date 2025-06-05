ba0c507687b1ef89fc60ab5bcaf097103d765958
naf-mughal <nafeelaaqib@gmail.com>
2025-06-05 19:53:24 +0500

    commit

--- apps/client/src/lib/eden.tsx
+++ apps/client/src/lib/eden.tsx
@@ -5,7 +5,7 @@ import type { App } from '@server/app'
 
 export const Client = () => {
     const { token } = useToken()
-    const client = treaty<App>('http://localhost:8080', {
+    const client = treaty<App>(process.env.NEXT_PUBLIC_API_URL!, {
         headers: {
             Authorization: token ? `Bearer ${token}` : '',
         },
